744f8bf0c4e2456f80a8b48fbe122aa9f56dd6ed
Maker0N <makerov@e1.ru>
2021-12-28 23:47:20 +0500

    fix devServer for routing

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -13,7 +13,7 @@ const config = {
   output: {
     filename: 'js/[name].[contenthash].js',
     path: resolve(__dirname, 'dist'),
-    publicPath: '',
+    publicPath: '/',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
@@ -34,6 +34,7 @@ const config = {
         errors: true,
       },
     },
+    historyApiFallback: true,
   },
   module: {
     rules: [
